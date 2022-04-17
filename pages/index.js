db52459e1fa26520270195291f6ad9a8c7402fcc
Joseph Bacal <bacalj@gmail.com>
2022-04-17 13:05:39 -0400

    installing anime cleanuop template

--- pages/index.js
+++ pages/index.js
@@ -15,36 +15,10 @@ export default function Home() {
         <h1 className={styles.title}>
           The Power of <a href="https://nextjs.org">Truths</a>
         </h1>
-
-        {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-        </div> */}
-
       </main>
 
       <footer className={styles.footer}>
         The footer
-        {/* <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a> */}
       </footer>
     </div>
   )
