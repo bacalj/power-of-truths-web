d7f0dbc1eb5969c27b2973f889d05d514b09a5a7
Joseph Bacal <bacalj@gmail.com>
2022-04-17 13:12:30 -0400

    getting an example component working

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Example from '../components/example'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Example texto="hello"></Example>
         <h1 className={styles.title}>
           The Power of <a href="https://nextjs.org">Truths</a>
         </h1>
