13f546ce3d11de33c9b2a5f82d87514d912f8b7d
Joe Bacal <bacalj@gmail.com>
2023-02-24 10:55:42 -0500

    fix ticket links

--- components/navbar.js
+++ components/navbar.js
@@ -47,7 +47,7 @@ export default function Navbar(){
                     <Link href="/#about">About</Link>
                 </div>
                 <div className="navbar-register-link">
-                    <Link href="/#register">Register</Link>
+                    <Link href="/tickets">Register</Link>
                 </div>
                 <div>
                     <Link href="/#schedule">Schedule</Link>
--- pages/tickets.js
+++ pages/tickets.js
@@ -24,16 +24,16 @@ export default function Tickets() {
         <div className='tickets-wrapper'>
           <div className='stripe-pass-one'>
             <h2>Festival Pass</h2>
-            <p>With a Festival Pass, you will have access to all workshop, keynote, and panel sessions on Saturday (10 am-5 pm) and Sunday (11 am-4 pm). Lunch on Saturday is included with the Full Festival Pass.
+            <p>With a Festival Pass, you will have access to all workshop, keynote, and panel sessions on Saturday (10 am-5 pm) and Sunday (11 am-4 pm). Lunch on Saturday is included with the Festival Pass.
             </p>
-
-            <p className='ticket'><a className='ticekt ticket-link' href='https://buy.stripe.com/7sIaHF31p9tK1wI4go'>Purchase Festival Pass   $40</a></p>
+            <h3>Festival Pass</h3>
+            <p className='ticket'><a className='ticekt ticket-link' href='https://buy.stripe.com/7sIaHF31p9tK1wI4go'>Purchase $40</a></p>
           </div>
           <div className='stripe-pass-two'>
             <h2>Festival Pass & Know the Ledge: Hiphop History Live</h2>
             <p>The Power of Truths Arts and Education Festival presents Know the Ledge: Hiphop History Live. Join us for an evening of storytelling, music, and dance as we bring together amazing artists from multiple disciplines to tell important and often overlooked stories from the history of our country. Part theatre, part history lesson, part Hiphop music, join artists Akrobatik; members of Fire in Little Africa—Written Quincey, Krisheena Suarez, and iamDES; Marcia Gomes; Khalif Neville; Dutch Rebelle and more for an intense and entertaining trip through history.</p>
-
-            <p className='ticket'><a className='ticket ticket-link' href='https://buy.stripe.com/14kbLJatR35m1wIaEL'>Purchase Festival Pass and Ticket to Know The Ledge   $55</a></p>
+            <h3>Festival Pass & Ticket to Know The Ledge</h3>
+            <p className='ticket'><a className='ticket ticket-link' href='https://buy.stripe.com/14kbLJatR35m1wIaEL'>Purchase $55</a></p>
 
           </div>
         </div>
