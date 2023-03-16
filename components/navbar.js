deca481fde4406baec8ff1998772437a2da9c9a0
Joe Bacal <bacalj@gmail.com>
2023-03-16 07:31:06 -0400

    correct other link

--- components/navbar.js
+++ components/navbar.js
@@ -47,7 +47,7 @@ export default function Navbar(){
                     <Link href="/#about">About</Link>
                 </div>
                 <div className="navbar-register-link">
-                    <Link href="https://bombyx.live/power-of-truths">Register</Link>
+                    <Link href="https://bombyx.live/events/power-of-truths-2023/">Register</Link>
                 </div>
                 <div>
                     <Link href="/#schedule">Schedule</Link>
